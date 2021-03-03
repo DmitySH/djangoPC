bc3a17488bc5508c633caf77495223db474745dc
dmitry <di.im2015@yandex.ru>
2021-03-03 23:27:50 +0300

    'last'

--- blog/views.py
+++ blog/views.py
@@ -14,12 +14,12 @@ def post_new(request):
     if request.method == "POST":
    		form = PostForm(request.POST)
    		if form.is_valid():
-	    post = form.save(commit=False)
-	    post.author = request.user
-	    post.published_date = timezone.now()
-	    post.save()
-	else:
-   		form = PostForm()
+		    post = form.save(commit=False)
+		    post.author = request.user
+		    post.published_date = timezone.now()
+		    post.save()
+		else:
+	   		form = PostForm()
     return redirect('post_detail', pk=post.pk)
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
